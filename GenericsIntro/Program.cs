22daef0bf0fa702532982c2a8f01786c7b056379
ACER <ACER@DESKTOP-TRKNRSV>
2021-03-10 11:54:57 +0300

    Add project files.

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Türkan");
+
+            List<string> liste = new List<string>();
+            Console.WriteLine(liste.Count);
+
+
+            
+        }
+    }
+}
